7c33dca48c39752eddd5ee6be078948f9f9908eb
karthikkumargundu <gundukarthik.kumar@ltts.com>
2021-03-16 09:07:42 +0530

    commit

---
+++ DemoSpring/src/main/java/com/ltts/DemoSpring/Student.java
@@ -0,0 +1,38 @@
+package com.ltts.DemoSpring;
+
+public class Student {
+		private int studentid;
+		private String studentname;
+		private String studentaddress;
+		public int getStudentid() {
+			return studentid;
+			
+		}
+		public Student(int studentid, String studentname, String studentaddress) {
+			super();
+			this.studentid = studentid;
+			this.studentname = studentname;
+			this.studentaddress = studentaddress;
+		}
+		public void setStudentid(int studentid) {
+			this.studentid = studentid;
+		}
+		public String getStudentname() {
+			return studentname;
+		}
+		public void setStudentname(String studentname) {
+			this.studentname = studentname;
+		}
+		public String getStudentaddress() {
+			return studentaddress;
+		}
+		public void setStudentaddress(String studentaddress) {
+			this.studentaddress = studentaddress;
+		}
+		@Override
+		public String toString() {
+			return "Student [studentid=" + studentid + ", studentname=" + studentname + ", studentaddress="
+					+ studentaddress + "]";
+		}
+		
+}
